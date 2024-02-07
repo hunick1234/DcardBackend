cae38ceb97b4293c4bf969bc958560ecde6c7453
nick <hunick1234@gmail.com>
2024-02-07 21:39:16 +0800

    feat: support http method

--- Myhttp/server.go
+++ Myhttp/server.go
@@ -1,45 +1,121 @@
 package Myhttp
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type Server struct {
 	*http.Server
+	*MyMux
 }
 
-type HttpMethod interface {
-	Get(path string, router http.HandlerFunc)
-	Post(path string, router http.HandlerFunc)
+type Method string
+type Path string
+type MyMux struct {
+	method map[Method]map[Path]*Router
 }
 
-func (s *Server) SetRouter(path string, router http.HandlerFunc) {
+type Router struct {
+	path    string
+	method  Method
+	handler http.HandlerFunc
+}
 
-	http.HandleFunc(path, router)
+func NewServer() Server {
+	return Server{
+		Server: &http.Server{
+			Addr: ":8080",
+		},
+		MyMux: &MyMux{
+			method: make(map[Method]map[Path]*Router, 10),
+		},
+	}
+}
+
+// todo check path first byte is '/'
+func (s *Server) setRouter(path string, method Method, handler http.HandlerFunc) {
+	if s.MyMux.method[method] == nil {
+		s.MyMux.method[method] = make(map[Path]*Router, 10)
+	}
+
+	s.method[method][Path(path)] = &Router{
+		path:    path,
+		method:  method,
+		handler: handler,
+	}
+}
+
+func useRouter(m *MyMux, r *http.Request) http.Handler {
+	if m.method[Method(r.Method)] == nil {
+		return nil
+	}
+
+	if m.method[Method(r.Method)][Path(r.URL.Path)] == nil {
+		return nil
+	}
+	return m.method[Method(r.Method)][Path(r.URL.Path)].handler
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.RequestURI == "*" {
+		if r.ProtoAtLeast(1, 1) {
+			w.Header().Set("Connection", "close")
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	handler := useRouter(s.MyMux, r)
+	if handler == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	handler.ServeHTTP(w, r)
 }
 
-func (s *Server) Start() error {
-	return http.ListenAndServe(s.Addr, nil)
+func (s *Server) Start() {
+	go func() {
+		if err := http.ListenAndServe(s.Addr, s); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	log.Printf("Server is running on port %s", s.Addr)
+	waitSignal()
+	greatFullShutdown(s)
 }
 
 func (s *Server) Stop() error {
 	return s.Close()
 }
 
-func (s *Server) Method() HttpMethod {
-	return s
+func (s *Server) Get(path string, handler http.HandlerFunc) {
+	s.setRouter(path, http.MethodGet, handler)
 }
 
-func (s *Server) Get(path string, router http.HandlerFunc) {
-
-	http.HandleFunc(path, router)
+func (s *Server) Post(path string, handler http.HandlerFunc) {
+	s.setRouter(path, http.MethodPost, handler)
 }
 
-func (s *Server) Post(path string, router http.HandlerFunc) {
+func waitSignal() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Server is shutting down...")
+}
 
-	http.HandleFunc(path, router)
+func greatFullShutdown(server *Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 }
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/hunick1234/DcardBackend/Myhttp"
 	v1 "github.com/hunick1234/DcardBackend/api/v1"
 )
@@ -11,20 +8,12 @@ import (
 var server Myhttp.Server
 
 func init() {
-	server = Myhttp.Server{
-		Server: &http.Server{
-			Addr: ":8080",
-		},
-	}
+	server = Myhttp.NewServer()
+	server.Addr = ":8080"
 }
 
 func main() {
-
-	server.SetRouter("/post/v1/ads", v1.CreatAD)
-	server.SetRouter("/get/v1/ads", v1.GetAD)
-	fmt.Println("Server is running on port " + server.Addr)
-	err := server.Start()
-	if err != nil {
-		fmt.Println(err)
-	}
+	server.Get("/api/v1/ads", v1.GetAD)
+	server.Post("/api/v1/ads", v1.CreatAD)
+	server.Start()
 }
