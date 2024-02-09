17029bcbbc30f1e4b18abf81f5a39b175195d0cc
nick <hunick1234@gmail.com>
2024-02-09 12:30:16 +0800

    add: validation for conditions

--- model/ad.go
+++ model/ad.go
@@ -1,16 +1,44 @@
 package model
 
+import (
+	"errors"
+)
+
 type AD struct {
-	Title     string    `json:"title"`
-	EndAt     string    `json:"endAt"`
-	StartAt   string    `json:"startAt"`
-	Timestamp string    `json:"timestamp"`
-	Conditons Conditons `json:"conditons"`
+	Title      string `json:"title"`
+	EndAt      string `json:"endAt"`
+	StartAt    string `json:"startAt"`
+	Timestamp  string `json:"timestamp"`
+	Conditions `json:"conditions"`
 }
 
-type Conditons struct {
+type Conditions struct {
 	AgeStart int      `json:"ageStart"`
 	AgeEnd   int      `json:"ageEnd"`
+	Gender   string   `json:"gender"`
 	Country  []string `json:"country"`
 	Platform []string `json:"platform"`
+}
+
+func (ad *AD) IsValidConditons() error {
+	if !ad.Conditions.isValidAge() {
+		return errors.New("Invalid age")
+	}
+	return nil
+}
+
+func (ad *Conditions) isValidAge() bool {
+	return true
+}
+
+func (ad *Conditions) isValidGender() bool {
+	return true
+}
+
+func (ad *Conditions) isValidCountry() bool {
+	return true
+}
+
+func (ad *Conditions) isValidPlatform() bool {
+	return true
 }
