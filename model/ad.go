19582ed5dc0f1fe6913223f0dd599a423a351c69
nick <hunick1234@gmail.com>
2024-02-23 15:37:50 +0800

    feat: More complete ad verification ----- - Add new ad filied - New struct ResponseAd, Conditions, AdQuery - Add vaildation Rule for ad parameter

--- model/ad.go
+++ model/ad.go
@@ -1,44 +1,249 @@
 package model
 
 import (
-	"errors"
+	"fmt"
+	"time"
+
+	"github.com/hunick1234/DcardBackend/validation"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type AD struct {
-	Title      string `json:"title"`
-	EndAt      string `json:"endAt"`
-	StartAt    string `json:"startAt"`
-	Timestamp  string `json:"timestamp"`
-	Conditions `json:"conditions"`
+	Title          string     `json:"title" bson:"title"`
+	EndAt          string     `json:"endAt" bson:"end_at"`
+	StartAt        string     `json:"startAt" bson:"start_at"`
+	Timestamp      int64      `json:"timestamp" bson:"timestamp"`
+	StartTimestamp int64      `json:"startTimestamp" bson:"start_timestamp"`
+	EndTimestamp   int64      `json:"endTimestamp" bson:"end_timestamp"`
+	IsStart        bool       `json:"isStart" bson:"is_start"`
+	IsEnd          bool       `json:"isEnd" bson:"is_end"`
+	Conditions     Conditions `json:"conditions" bson:"conditions"`
+}
+
+type ResponseAd struct {
+	Title string    `json:"title" bson:"title"`
+	EndAt time.Time `json:"endAt" bson:"endAt"`
 }
 
 type Conditions struct {
-	AgeStart int      `json:"ageStart"`
-	AgeEnd   int      `json:"ageEnd"`
-	Gender   string   `json:"gender"`
+	AgeStart int      `json:"ageStart" bson:"age_start"`
+	AgeEnd   int      `json:"ageEnd" bson:"age_end"`
+	Gender   []string `json:"gender" bson:"gender"`
+	Country  []string `json:"country" bson:"country"`
+	Platform []string `json:"platform" bson:"platform"`
+}
+
+type AdQuery struct {
+	Offset   int      `json:"offset"`
+	Limit    int      `json:"limit"`
+	Age      int      `json:"age"`
+	Gender   []string `json:"gender"`
 	Country  []string `json:"country"`
 	Platform []string `json:"platform"`
 }
 
-func (ad *AD) IsValidConditons() error {
-	if !ad.Conditions.isValidAge() {
-		return errors.New("Invalid age")
+var TestValidationAdSchema = bson.M{
+	"$jsonSchema": bson.M{
+		"bsonType": "object",
+		"required": []string{"end_at", "start_at", "timestamp", "conditions", "title"},
+		"properties": bson.M{
+			"endAt": bson.M{
+				"bsonType":    "date",
+				"description": "must be a date and is required",
+			},
+			"conditions": bson.M{
+				"bsonType":    "object",
+				"description": "must be an object and is required",
+				"required":    []string{"age_start", "age_end", "gender", "country", "platform"},
+				"properties": bson.M{
+					"age_start": bson.M{
+						"bsonType":    "int",
+						"minimum":     0,
+						"maximum":     100,
+						"description": "must be an integer and is required",
+					},
+					"age_end": bson.M{
+						"bsonType":    "int",
+						"minimum":     0,
+						"maximum":     100,
+						"description": "must be an integer and is required",
+					},
+				},
+			},
+		},
+	},
+}
+
+func NewAd() AD {
+	return AD{
+		Title:          "",
+		EndAt:          "",
+		StartAt:        "",
+		Timestamp:      time.Now().Unix(),
+		StartTimestamp: 0,
+		EndTimestamp:   0,
+		IsStart:        false,
+		IsEnd:          false,
+		Conditions:     NewConditions(),
 	}
-	return nil
 }
 
-func (ad *Conditions) isValidAge() bool {
+func NewConditions() Conditions {
+	return Conditions{
+		AgeStart: 0,
+		AgeEnd:   100,
+		Gender:   []string{},
+		Country:  []string{},
+		Platform: []string{},
+	}
+}
+
+func NewAdQuery() AdQuery {
+	return AdQuery{
+		Offset:   0,
+		Limit:    10,
+		Age:      0,
+		Gender:   []string{},
+		Country:  []string{},
+		Platform: []string{},
+	}
+}
+
+func (ad *AD) IsValidAd() bool {
+
+	if !ad.isValidStartAt() || !ad.isValidEndAt() || !ad.isValidAdLiveTime() {
+		return false
+	}
 	return true
 }
 
-func (ad *Conditions) isValidGender() bool {
+func (ad *AD) isValidStartAt() bool {
+	startAtTime, err := time.Parse(time.RFC3339, ad.StartAt)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	ad.StartTimestamp = startAtTime.Unix()
 	return true
 }
 
-func (ad *Conditions) isValidCountry() bool {
+func (ad *AD) isValidEndAt() bool {
+
+	endAtTime, err := time.Parse(time.RFC3339, ad.EndAt)
+	if err != nil {
+		return false
+	}
+	ad.EndTimestamp = endAtTime.Unix()
 	return true
 }
 
-func (ad *Conditions) isValidPlatform() bool {
+func (ad *AD) isValidAdLiveTime() bool {
+	if ad.StartTimestamp > ad.EndTimestamp {
+		return false
+	}
+	return true
+}
+
+func (conditions *Conditions) IsValidConditions() bool {
+	if !conditions.isValidAge() || !conditions.isValidGender() || !conditions.isValidCountry() || !conditions.isValidPlatform() {
+		return false
+	}
+	return true
+}
+
+func (conditions *Conditions) isValidAge() bool {
+	if conditions.AgeStart < 0 || conditions.AgeStart > 100 {
+		return false
+	}
+
+	if conditions.AgeEnd < 0 || conditions.AgeEnd > 100 {
+		return false
+	}
+
+	if conditions.AgeStart > conditions.AgeEnd {
+		return false
+	}
+	return true
+}
+
+func (conditions *Conditions) isValidGender() bool {
+	for _, v := range conditions.Gender {
+		if validation.Gender[v] {
+			return false
+		}
+	}
+	return true
+}
+
+func (conditions *Conditions) isValidCountry() bool {
+	for _, v := range conditions.Country {
+		if !validation.ISO3166[v] {
+			return false
+		}
+	}
+	return true
+}
+
+func (conditions *Conditions) isValidPlatform() bool {
+	for _, v := range conditions.Platform {
+		if validation.Plateform[v] {
+			return false
+		}
+	}
+	return true
+}
+
+func (query *AdQuery) IsValidAdQuery() bool {
+	if !query.isValidOffset() || !query.isValidLimit() || !query.isValidAge() || !query.isValidGender() || !query.isValidCountry() || !query.isValidPlatform() {
+		return false
+	}
+	return true
+}
+
+func (query *AdQuery) isValidOffset() bool {
+	if query.Offset < 0 {
+		return false
+	}
+	return true
+}
+
+func (query *AdQuery) isValidLimit() bool {
+	if query.Limit < 0 || query.Limit > 100 {
+		return false
+	}
+	return true
+}
+
+func (query *AdQuery) isValidAge() bool {
+	if query.Age < 0 || query.Age > 100 {
+		return false
+	}
+	return true
+}
+
+func (query *AdQuery) isValidGender() bool {
+	for _, v := range query.Gender {
+		if validation.Gender[v] {
+			return false
+		}
+	}
+	return true
+}
+
+func (query *AdQuery) isValidCountry() bool {
+	for _, v := range query.Country {
+		if !validation.ISO3166[v] {
+			return false
+		}
+	}
+	return true
+}
+
+func (query *AdQuery) isValidPlatform() bool {
+	for _, v := range query.Platform {
+		if validation.Plateform[v] {
+			return false
+		}
+	}
 	return true
 }
---
+++ validation/ad.go
@@ -0,0 +1,35 @@
+package validation
+
+var _ISO3166 = []string{"AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW"}
+var ISO3166 = tranlat_ISO3166ToMap()
+var Plateform = map[string]bool{
+	"ios":     true,
+	"android": true,
+	"web":     true,
+}
+var Gender = map[string]bool{
+	"M": true,
+	"F": true,
+}
+
+type Iso3166 struct{}
+
+func tranlat_ISO3166ToMap() map[string]bool {
+	m := make(map[string]bool)
+	for _, v := range _ISO3166 {
+		m[v] = true
+	}
+	return m
+}
+
+func (i *Iso3166) IsValidIso3166(iso3166 string) bool {
+	return ISO3166[iso3166]
+}
+
+func IsValidPlateform(plateform string) bool {
+	return Plateform[plateform]
+}
+
+func IsValidGender(gender string) bool {
+	return Gender[gender]
+}
